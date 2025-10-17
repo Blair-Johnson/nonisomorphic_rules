719f1cae97b59dd46ca19c518d6bd58ee3e43d2a
Blair-Johnson <blairjohnson@gatech.edu>
2025-10-17 13:50:54 -0400

    generator for prolog facts complete

--- gen.py
+++ gen.py
@@ -36,7 +36,35 @@ def filter_search_space(query_graphs:list[nx.DiGraph], fact_graphs:list[nx.DiGra
                 valid_search_pairs[i].append(j)
     return valid_search_pairs
 
-def find_all_facts(prolog:Prolog(), graphs:dict[str,nx.DiGraph]) -> list[str]:
+def invert_graph_index(graphs:dict[str,list[nx.DiGraph]]) -> dict[nx.DiGraph,str]:
+    # graph names g{n_nodes}_{global_index}
+    rev_index = {}
+    global_idx = 0
+    for k,v in graphs.items():
+        for g in v:
+            rev_index[g] = k + f"_{global_idx}"
+            global_idx += 1
+    return rev_index
+
+def construct_fact_strings(facts:list, graphs:dict[str,list[nx.DiGraph]], rev_index:dict[nx.DiGraph,str]) -> list[str]:
+    output_facts = []
+    for def_graph, bk_graph, vertices in facts:
+        rule_head = rev_index[def_graph]
+        vertex_suffix = rev_index[bk_graph]
+        new_facts = [f"{rule_head}({x}_{vertex_suffix},{y}_{vertex_suffix})" for x,y in vertices]
+        output_facts += new_facts
+    return output_facts
+
+def create_graph_facts(rev_index:dict[nx.DiGraph,str]) -> list[str]:
+    output_facts = []
+    for graph,name in rev_index.items():
+        n_nodes = len(graph.nodes())
+        output_facts.append(f"{name}(v0_{name},v{n_nodes-1}_{name})")
+        for i,j in graph.edges():
+            output_facts.append(f"di_edge(v{i}_{name},v{j}_{name})")
+    return output_facts
+
+def find_all_facts(prolog:Prolog(), graphs:dict[str,list[nx.DiGraph]]) -> list[str]:
     gn_ordered = sorted(list(graphs.keys()))
     all_tasks = []
     for i in range(len(gn_ordered)):
@@ -50,11 +78,19 @@ def find_all_facts(prolog:Prolog(), graphs:dict[str,nx.DiGraph]) -> list[str]:
     extra_facts = []
     for q_graph, f_graph in tqdm(comps):
         solns = find_SAT_pairs(prolog, q_graph, f_graph)
-        extra_facts.append(solns)
-    return extra_facts
+        if len(solns) > 0:
+            extra_facts.append((q_graph,f_graph,solns))
+    rev_index = invert_graph_index(graphs)
+    extra_facts = construct_fact_strings(extra_facts, graphs, rev_index)
+    base_facts = create_graph_facts(rev_index)
+    all_facts = base_facts + extra_facts
+    return all_facts
 
 if __name__ == '__main__':
-    graphs = {f'g{n}':load_digraph6_file(f'./digraphs/g{n}cd.g6')[f'g{n}'] for n in range(2,6)}
+    graphs = {f'g{n}':load_digraph6_file(f'./digraphs/g{n}cd.g6')[f'g{n}'] for n in range(3,6)}
     prolog = Prolog()
     solns = find_SAT_pairs(prolog, graphs['g3'][0], graphs['g5'][0])
-    find_all_facts(prolog, graphs)
+    all_facts = find_all_facts(prolog, graphs)
+    with open("bk.pl", "w") as f:
+        f.write('.\n'.join(all_facts))
+        f.write('.')
