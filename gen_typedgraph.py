03a1adc2478ff9250fcdbcc8103c63b96a526a4e
Blair-Johnson <blairjohnson@gatech.edu>
2025-10-24 00:46:19 -0400

    added typed edges

--- gen_typedgraph.py
+++ gen_typedgraph.py
@@ -1,24 +1,39 @@
 import time
 import networkx as nx
+import random
 from tqdm import tqdm
 from pyswip import Prolog
 from digraph6 import load_digraph6_file
 
-def create_prolog_rule(G:nx.DiGraph, name:str='query') -> str:
-    atoms = [f"di_edge(Z{i},Z{j})" for i,j in G.edges()]
-    atoms.append(f"all_distinct([{', '.join([f'Z{i}' for i in G.nodes()])}])")
+GLIDR_SEMANTICS = False
+N_PREDS = 1
+
+def create_prolog_rule(G:nx.DiGraph, name:str='query', distinct=True) -> str:
+    atoms = [f"di_edge_{dat['pred']}(Z{i},Z{j})" for i,j,dat in G.edges(data=True)]
+    if distinct:
+        if not GLIDR_SEMANTICS:
+            atoms.append(f"all_distinct([{', '.join([f'Z{i}' for i in G.nodes()])}])")
+        else:
+            # NOTE: Adds equivalent constraints to GLIDR's "restrict_existential_grounding" mode
+            for j in list(G.nodes())[1:-1]:
+                atoms.append(f"all_distinct([Z0, Z{j}])")
+                atoms.append(f"all_distinct([Z{len(G.nodes())-1}, Z{j}])")
     head = name + f"(Z0, Z{len(G.nodes())-1})"
     return head + " :- " + ", ".join(atoms)
 
-def find_SAT_pairs(prolog:Prolog, query_graph:nx.DiGraph, facts_graph:nx.DiGraph) -> list:
+def find_SAT_pairs(prolog:Prolog, query_graph:nx.DiGraph, facts_graph:nx.DiGraph, distinct=True) -> list:
     # ingest background facts
-    prolog.assertz("all_distinct(L) :- sort(L,S), length(L,N), length(S,N)")
-    for i,j in facts_graph.edges():
-        prolog.assertz(f"di_edge(v{i}, v{j})")
-    rule = create_prolog_rule(query_graph)
+    if distinct:
+        prolog.assertz("all_distinct(L) :- sort(L,S), length(L,N), length(S,N)")
+    seen = set()
+    for i,j,dat in facts_graph.edges(data=True):
+        prolog.assertz(f"di_edge_{dat['pred']}(v{i}, v{j})")
+        seen.add(dat['pred'])
+    rule = create_prolog_rule(query_graph, distinct=distinct)
     prolog.assertz(rule)
     solns = list(set([(soln['X'],soln['Y']) for soln in prolog.query("query(X,Y)")]))
-    prolog.retractall("di_edge(_,_)")
+    for p in seen:
+        prolog.retractall(f"di_edge_{p}(_,_)")
     prolog.retractall("query(_,_)")
     prolog.retractall("all_distinct(_)")
     return solns
@@ -60,11 +75,11 @@ def create_graph_facts(rev_index:dict[nx.DiGraph,str]) -> list[str]:
     for graph,name in rev_index.items():
         n_nodes = len(graph.nodes())
         output_facts.append(f"{name}(v0_{name},v{n_nodes-1}_{name})")
-        for i,j in graph.edges():
-            output_facts.append(f"di_edge(v{i}_{name},v{j}_{name})")
+        for i,j,dat in graph.edges(data=True):
+            output_facts.append(f"di_edge_{dat['pred']}(v{i}_{name},v{j}_{name})")
     return output_facts
 
-def find_all_facts(prolog:Prolog(), graphs:dict[str,list[nx.DiGraph]]) -> list[str]:
+def find_all_facts(prolog:Prolog(), graphs:dict[str,list[nx.DiGraph]], distinct=True) -> list[str]:
     gn_ordered = sorted(list(graphs.keys()))
     all_tasks = []
     for i in range(len(gn_ordered)):
@@ -77,7 +92,7 @@ def find_all_facts(prolog:Prolog(), graphs:dict[str,list[nx.DiGraph]]) -> list[s
     print(f'Performing {len(comps)} graph comparisons to check for subgraphs.')
     extra_facts = []
     for q_graph, f_graph in tqdm(comps):
-        solns = find_SAT_pairs(prolog, q_graph, f_graph)
+        solns = find_SAT_pairs(prolog, q_graph, f_graph, distinct=distinct)
         if len(solns) > 0:
             extra_facts.append((q_graph,f_graph,solns))
     rev_index = invert_graph_index(graphs)
@@ -86,11 +101,15 @@ def find_all_facts(prolog:Prolog(), graphs:dict[str,list[nx.DiGraph]]) -> list[s
     all_facts = base_facts + extra_facts
     return all_facts
 
+def add_edge_types(graph:nx.DiGraph) -> nx.DiGraph:
+    edge_attributes = {(u,v): random.randint(0, N_PREDS-1) for u,v in graph.edges()}
+    nx.set_edge_attributes(graph, edge_attributes, name="pred")
+    return graph
+
 if __name__ == '__main__':
-    graphs = {f'g{n}':load_digraph6_file(f'./digraphs/g{n}cd.g6')[f'g{n}'] for n in range(3,6)}
+    graphs = {f'g{n}':list(map(lambda x: add_edge_types(x), load_digraph6_file(f'./digraphs/g{n}cd.g6')[f'g{n}'])) for n in range(3,6)}
     prolog = Prolog()
-    solns = find_SAT_pairs(prolog, graphs['g3'][0], graphs['g5'][0])
-    all_facts = find_all_facts(prolog, graphs)
-    with open("bk.pl", "w") as f:
+    all_facts = find_all_facts(prolog, graphs, distinct=False)
+    with open(f"bk_{N_PREDS}_random_preds.pl", "w") as f:
         f.write('.\n'.join(all_facts))
         f.write('.')
