d6741b43c0e33f558dedafb3ba973428acc4491b
Blair-Johnson <blairjohnson@gatech.edu>
2025-10-25 18:18:18 -0400

    make edge predicates dynamic

--- lib.pl
+++ lib.pl
@@ -1,4 +1,11 @@
+% define uniqueness constraint
 all_unique([]).
 all_unique([X|Xs]) :-
     maplist(dif(X), Xs),
     all_unique(Xs).
+
+% declare edge predicates dynamic so that they can be asserted and retracted after facts
+:- dynamic(di_edge_0/2).
+:- dynamic(di_edge_1/2).
+:- dynamic(di_edge_2/2).
+:- dynamic(di_edge_3/2).
